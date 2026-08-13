a55bce1b0b0c42dce92c9fae9ec0b4114e254e0d
teatime77 <teatime77@live.jp>
2026-08-13 22:47:07 +0900

    use vite server

--- ts/index.ts
+++ ts/index.ts
@@ -1,4 +1,5 @@
 export { isGreek, parseMath, makeIdToTermMap, Rational, Term, ConstNum, App, RefVar, operator, Path } from "./parser.js";
 export { renderKatexSub, SyntaxError } from "./parser_util.js";
-export { showFlow, Highlightable, makeNodeTextByApp } from "./tex.js";
+export { showFlow, makeNodeTextByApp } from "./tex.js";
+export type { Highlightable } from "./tex.js";
 export { isLetter } from "./lex.js";
